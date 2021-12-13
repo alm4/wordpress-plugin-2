3f8415b1c18da29e5d4f8df25b825b58592ab09e
Arthur Magno <arthurmagno@arthurmagno.com>
2021-12-13 07:46:26 -0300

    Main file

---
+++ menu_suspenso.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Menu Suspenso
+Author: Arthur Magno
+Version: 0.0.1
+Author URI: www.arthurmagno.com
+License: GPLv2 or later
+Text Domain: menu_suspenso
+Plugin URI: www.arthurmagno.com
+Description: Menu Suspenso.
+*/
+
+?>
